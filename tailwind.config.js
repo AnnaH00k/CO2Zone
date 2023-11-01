0b9b4d8f084c537c032dbda1bfc205b27630775c
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2023-11-01 20:12:17 +0100

    Implement Tailwind,Bootstrap &CSS #2 (design home)

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./home.html"],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+        'bg': '#1E2A52',
+        'blueExtraLight': '#D7D8EF',
+        'blueLight': '#7C7EAE',
+        'blueDark': '#41426C',
+        'blueExtraDark': '#111F43',
+      },
+    },
+  },
+  plugins: [],
+}
+
