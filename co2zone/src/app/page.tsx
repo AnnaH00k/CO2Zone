6fd6b097346ab7d8eefacbe83967134f7fabded1
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2023-11-06 23:34:40 +0100

    #8 tabelle mit gemappten werten

--- co2zone/src/app/page.tsx
+++ co2zone/src/app/page.tsx
@@ -1,7 +1,43 @@
+"use client";
 import Image from "next/image";
 import MenuHeader from "./components/menuHeader";
+import jsonData from "./Data/table.json";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tableData, setTableData] = useState<TableData[]>([]);
+
+  // Ensure that the interface is defined outside the component
+  interface TableData {
+    country: string;
+    code?: string;
+    CO2_emissions: { [key: string]: number };
+    company?: string;
+  }
+
+  // Fetch the data on component mount
+  useEffect(() => {
+    const formattedData: TableData[] = jsonData.table.map((item: any) => ({
+      country: item.country || "",
+      code: item.code || "",
+      CO2_emissions: item.CO2_emissions || {},
+      company: item.company || "",
+    }));
+    setTableData(formattedData);
+  }, [jsonData]);
+
+ // Extract the years from the data to dynamically generate table headers
+const allYears = tableData.reduce((acc, curr) => {
+  const years = Object.keys(curr.CO2_emissions);
+  years.forEach((year) => {
+    if (!acc.includes(year)) {
+      acc.push(year);
+    }
+  });
+  return acc;
+}, [] as string[]);
+
+
   return (
     <>
       <MenuHeader />
@@ -16,44 +52,40 @@ export default function Home() {
           </h1>
         </div>
 
+
         <div className="justify-center align-center flex items-center w-[100vw] mt-[3vh]">
-          <section className="mt-4 w-[80vw]">
+          <section className="mt-4 w-[90vw] sm:w-[80vw]">
             <h2 className="text-lg sm:text-4xl align-center flex justify-center p-3  text-blueDark bg-blueExtraLight">
-              CO2-Emissionsübersicht
+              CO2-Emission-Overview
             </h2>
-            <div className="list-group-item mb-0">
-              <table className="bg-blueLight w-[80vw] text-blueExtraDark text-left text-sm sm:text-xl ">
+            <div className="list-group-item mb-0" style={{ overflowX: "auto", width: "w-[90vw] sm:w-[80vw]" }}>
+              <table className="bg-blueLight w-[90vw] sm:w-[80vw] text-blueExtraDark text-left text-sm sm:text-xl mb-4">
                 <thead>
                   <tr className="border border-blueExtraLight border-solid border-1">
                     <th className="border border-blueExtraLight border-solid border-1">x</th>
-                    <th>Land</th>
-                    <th>code</th>
-                    <th>Jahr0</th>
-                    <th>Jahr1</th>
+                    <th className="border border-blueExtraLight border-solid border-1">Land & company</th>
+                    <th className="border border-blueExtraLight border-solid border-1">code</th>
+                    {allYears.map((year) => (
+                      <th key={year} className="border border-blueExtraLight border-solid border-1">
+                        {year}
+                      </th>
+                    ))}
                   </tr>
                 </thead>
                 <tbody>
-                  <tr className="border border-blueExtraLight border-solid border-1">
-                    <td className="border border-blueExtraLight border-solid border-1">1</td>
-                    <td>China</td>
-                    <td>CHN</td>
-                    <td>10.064.147</td>
-                    <td>10.064.147</td>
-                  </tr>
-                  <tr className="border border-blueExtraLight border-solid border-1">
-                    <td className="border border-blueExtraLight border-solid border-1">2</td>
-                    <td>USA</td>
-                    <td>USA</td>
-                    <td>5.416.317</td>
-                    <td>5.416.317</td>
-                  </tr>
-                  <tr className="border border-blueExtraLight border-solid border-1">
-                    <td className="border border-blueExtraLight border-solid border-1">3</td>
-                    <td>Indien</td>
-                    <td>IND</td>
-                    <td>2.654.400</td>
-                    <td>2.654.400</td>
-                  </tr>
+                  {/* Loop through the tableData to create rows */}
+                  {tableData.map((item: TableData, index: number) => (
+                    <tr key={index} className="border border-blueExtraLight border-solid border-1">
+                      <td className="border border-blueExtraLight border-solid border-1">{index + 1}</td>
+                      <td className="border border-blueExtraLight border-solid border-1">{item.country || item.company}</td>
+                      <td className="border border-blueExtraLight border-solid border-1">{item.code || "-"}</td>
+                      {allYears.map((year) => (
+                        <td key={year} className="border border-blueExtraLight border-solid border-1">
+                          {item.CO2_emissions[year] || "-"}
+                        </td>
+                      ))}
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
