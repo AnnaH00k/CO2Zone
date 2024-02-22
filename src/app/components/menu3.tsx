ad839c12b1293d3eaf724441c78bab8af0d66f90
Anna H00k <annahook0@gmail.com>
2024-02-22 20:00:43 +0100

    spacing menu arabic

--- src/app/components/menu3.tsx
+++ src/app/components/menu3.tsx
@@ -6,7 +6,7 @@ export default function menu3() {
    
     const { t, i18n } = useTranslation();
     const isRTL = i18n.dir().toLowerCase() === 'rtl';
-    const marginClass = isRTL ? 'mr-[12vw]' : 'ml-[12vw]';
+    const marginClass = isRTL ? 'mr-[16vw]' : 'ml-[12vw]';
 
     return (
         <div className={`sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ${marginClass} text-blueExtraLight z-10`}>
--- src/app/components/menu4.tsx
+++ src/app/components/menu4.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 export default function menu4() {
   const { t, i18n } = useTranslation();
     const isRTL = i18n.dir().toLowerCase() === 'rtl';
-    const marginClass = isRTL ? 'mr-[20vw]' : 'ml-[20vw]';
+    const marginClass = isRTL ? 'mr-[25vw]' : 'ml-[20vw]';
 
   return (
     <div className={`sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ${marginClass} text-blueExtraLight z-10`}>
