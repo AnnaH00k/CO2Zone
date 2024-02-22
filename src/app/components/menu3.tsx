48f21dba5f1cf674cd921f36e06ac3f32c12f283
Anna H00k <annahook0@gmail.com>
2024-02-22 19:55:51 +0100

    menü arabisch fixen

--- src/app/components/menu3.tsx
+++ src/app/components/menu3.tsx
@@ -4,10 +4,12 @@ import { useTranslation } from "react-i18next";
 
 export default function menu3() {
    
-    const { t } = useTranslation("");
+    const { t, i18n } = useTranslation();
+    const isRTL = i18n.dir().toLowerCase() === 'rtl';
+    const marginClass = isRTL ? 'mr-[12vw]' : 'ml-[12vw]';
 
     return (
-        <div className='sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ml-[12vw] text-blueExtraLight z-10'>
+        <div className={`sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ${marginClass} text-blueExtraLight z-10`}>
             <a className='text-blueExtraLight no-underline text-sm md:text-lg sm:text-md w-auto my-[1vh] mx-[1vw]' href="/actions"> {t("menu3-1")}</a>
             <a className='text-blueExtraLight no-underline text-sm md:text-lg sm:text-md w-auto my-[1vh] mx-[1vw]' href="/actions#ActionCompanies">{t("menu3-2")}</a>
             <a className='text-blueExtraLight no-underline text-sm md:text-lg sm:text-md w-auto my-[1vh] mx-[1vw]' href="/actions#ActionGovernments"> {t("menu3-3")}</a>
--- src/app/components/menu4.tsx
+++ src/app/components/menu4.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 export default function menu4() {
-  const { t } = useTranslation("");
+  const { t, i18n } = useTranslation();
+    const isRTL = i18n.dir().toLowerCase() === 'rtl';
+    const marginClass = isRTL ? 'mr-[20vw]' : 'ml-[20vw]';
 
   return (
-    <div className="sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ml-[20vw] text-blueExtraLight z-10">
-      <a
+    <div className={`sm:relative fixed bg-blueDark hover:bg-blueExtraDark flex flex-col flex flex-col items-start w-auto h-auto  rounded-3xl border border-blueDark border-solid border-2 m-[1vh] ${marginClass} text-blueExtraLight z-10`}>
+    <a
         className="text-blueExtraLight hyphens-auto no-underline text-sm md:text-lg sm:text-md w-auto my-[1vh] mx-[1vw]"
         href="/interesting#CooperationOnClimateChange"
       >
